5e9d11fb518f8072d0f3d57ec4c3303431e849d4
David M. Lee <dlee@respoke.io>
2017-12-29 01:06:50 -0600

    More macOS fixes
     * Fix undeclared identifier bugs recently introduced in show sysinfo
     * In translate.c, we were taking the abs() of an unsigned value, which
       is probably not what we expected. We have to explicitly cast the
       difference of unsigned values to a signed value if we want it to
       be negative.
    
    Change-Id: I3900881168c570c4177bf394caf33562e5c67f94

--- main/asterisk.c
+++ main/asterisk.c
@@ -714,12 +714,6 @@ static int swapmode(int *used, int *total)
 	ast_free(swdev);
 	return 1;
 }
-#elif defined(HAVE_SYSCTL) && !defined(HAVE_SYSINFO)
-static int swapmode(int *used, int *total)
-{
-	*used = *total = 0;
-	return 1;
-}
 #endif
 
 #if defined(HAVE_SYSINFO) || defined(HAVE_SYSCTL)
@@ -729,10 +723,10 @@ static char *handle_show_sysinfo(struct ast_cli_entry *e, int cmd, struct ast_cl
 	uint64_t physmem, freeram;
 #if defined(HAVE_SYSINFO) || defined(HAVE_SWAPCTL)
 	uint64_t freeswap = 0;
+	int totalswap = 0;
 #endif
 	int nprocs = 0;
 	long uptime = 0;
-	int totalswap = 0;
 #if defined(HAVE_SYSINFO)
 	struct sysinfo sys_info;
 #elif defined(HAVE_SYSCTL)
@@ -740,7 +734,10 @@ static char *handle_show_sysinfo(struct ast_cli_entry *e, int cmd, struct ast_cl
 	struct vmtotal vmtotal;
 	struct timeval	boottime;
 	time_t	now;
-	int mib[2], pagesize, usedswap = 0;
+	int mib[2], pagesize;
+#if defined(HAVE_SWAPCTL)
+	int usedswap = 0;
+#endif
 	size_t len;
 #endif
 
@@ -799,9 +796,11 @@ static char *handle_show_sysinfo(struct ast_cli_entry *e, int cmd, struct ast_cl
 	len = sizeof(vmtotal);
 	sysctl(mib, 2, &vmtotal, &len, NULL, 0);
 	freeram = (vmtotal.t_free << pageshift);
+#if defined(HAVE_SWAPCTL)
 	/* generate swap usage and totals */
 	swapmode(&usedswap, &totalswap);
 	freeswap = (totalswap - usedswap);
+#endif
 	/* grab number of processes */
 #if defined(__OpenBSD__)
 	mib[0] = CTL_KERN;
